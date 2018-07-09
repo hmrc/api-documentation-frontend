914d6ef00ca748ea8130e6485b3150b0a0ce816c
Ed Jannoo <ed.jannoo@devability.co.uk>
2018-07-09 08:07:35 +0100

    APIS-1452 Use ListMap in JsonSchema to enable properties to render in the same order as defined in the schema

--- app/uk/gov/hmrc/apidocumentation/models/JsonSchema.scala
+++ app/uk/gov/hmrc/apidocumentation/models/JsonSchema.scala
@@ -16,36 +16,66 @@
 
 package uk.gov.hmrc.apidocumentation.models
 
+import play.api.data.validation.ValidationError
 import play.api.libs.functional.syntax._
+import play.api.libs.json.Json.fromJson
 import play.api.libs.json._
 
+import scala.collection.Seq
+import scala.collection.immutable.ListMap
+
+/*
+ * ListMap is used instead of Map so that when iterating the entries are returned in insertion order. This means
+ * that the documentation renders properties in the order defined in the schema rather than in a random order.
+ * This makes the documentation more usable.
+ */
 case class JsonSchema(description: Option[String] = None,
                       id: Option[String] = None,
                       `type`: Option[String] = None,
                       example: Option[String] = None,
                       title: Option[String] = None,
-                      properties: Map[String, JsonSchema] = Map(),
-                      patternProperties: Map[String, JsonSchema] = Map(),
+                      properties: ListMap[String, JsonSchema] = ListMap(), // See above regarding use of ListMap
+                      patternProperties: ListMap[String, JsonSchema] = ListMap(), // See above regarding use of ListMap
                       items: Option[JsonSchema] = None,
                       required: Seq[String] = Nil,
-                      definitions: Map[String, JsonSchema] = Map(),
+                      definitions: ListMap[String, JsonSchema] = ListMap(), // See above regarding use of ListMap
                       ref: Option[String] = None,
                       enum: Seq[String] = Nil,
                       oneOf: Seq[JsonSchema] = Nil,
                       pattern: Option[String] = None)
 
 object JsonSchema {
+
+  implicit def listMapReads[V](implicit formatV: Reads[V]): Reads[ListMap[String, V]] = new Reads[ListMap[String, V]] {
+    def reads(json: JsValue) = json match {
+      case JsObject(m) =>
+        type Errors = Seq[(JsPath, Seq[ValidationError])]
+        def locate(e: Errors, key: String) = e.map { case (path, validationError) => (JsPath \ key) ++ path -> validationError }
+
+        m.foldLeft(Right(ListMap.empty): Either[Errors, ListMap[String, V]]) {
+          case (acc, (key, value)) => (acc, fromJson[V](value)(formatV)) match {
+            case (Right(vs), JsSuccess(v, _)) => Right(vs + (key -> v))
+            case (Right(_), JsError(e)) => Left(locate(e, key))
+            case (Left(e), _: JsSuccess[_]) => Left(e)
+            case (Left(e1), JsError(e2)) => Left(e1 ++ locate(e2, key))
+          }
+        }.fold(JsError.apply, res => JsSuccess(res))
+
+      case _ => JsError(Seq(JsPath() -> Seq(ValidationError("error.expected.jsobject"))))
+    }
+  }
+
   implicit lazy val reads: Reads[JsonSchema] = (
     ( __ \ "description" ).readNullable[String] and
       ( __ \ "id" ).readNullable[String] and
       ( __ \ "type" ).readNullable[String] and
       ( __ \ "example" ).readNullable[String] and
       ( __ \ "title" ).readNullable[String] and
-      ( __ \ "properties" ).lazyReadNullable[Map[String,JsonSchema]](Reads.map[JsonSchema]).map(_.getOrElse(Map())) and
-      ( __ \ "patternProperties" ).lazyReadNullable[Map[String,JsonSchema]](Reads.map[JsonSchema]).map(_.getOrElse(Map())) and
+      ( __ \ "properties" ).lazyReadNullable[ListMap[String,JsonSchema]](listMapReads[JsonSchema]).map(_.getOrElse(ListMap())) and
+      ( __ \ "patternProperties" ).lazyReadNullable[ListMap[String,JsonSchema]](listMapReads[JsonSchema]).map(_.getOrElse(ListMap())) and
       ( __ \ "items" ).lazyReadNullable[JsonSchema](JsonSchema.reads) and
       ( __ \ "required" ).readNullable[Seq[String]].map(_.toSeq.flatten) and
-      ( __ \ "definitions" ).lazyReadNullable[Map[String,JsonSchema]](Reads.map[JsonSchema]).map(_.getOrElse(Map())) and
+      ( __ \ "definitions" ).lazyReadNullable[ListMap[String,JsonSchema]](listMapReads[JsonSchema]).map(_.getOrElse(ListMap())) and
       ( __ \ "$ref" ).readNullable[String] and
       ( __ \ "enum" ).readNullable[Seq[String]].map(_.toSeq.flatten) and
       ( __ \ "oneOf" ).lazyReadNullable[Seq[JsonSchema]](Reads.seq[JsonSchema]).map(_.toSeq.flatten) and
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,8 +3,8 @@ resolvers += Resolver.url("hmrc-sbt-plugin-releases",
 
 resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases/"
 
-addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "1.9.0")
-addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "1.0.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "1.10.0")
+addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "1.1.0")
 addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "0.10.0")
 
 addSbtPlugin("org.irundaia.sbt" % "sbt-sassify" % "1.4.6")
