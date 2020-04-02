c428d342ffbf7f5b1a8dafaae1e8d36cbcd980cf
Andy Spaven <=>
2020-04-02 10:38:58 +0100

    Revert "Merge pull request #203 from hmrc/APIS-4714"
    This reverts commit 03a9e81df10ce242966550e6a87f33f12b0799dc, reversing
    changes made to 995190f9237e8a7e5c7a894e0e3520eb645e68a6.

--- app/uk/gov/hmrc/apidocumentation/config/ApplicationConfig.scala
+++ app/uk/gov/hmrc/apidocumentation/config/ApplicationConfig.scala
@@ -17,7 +17,6 @@
 package uk.gov.hmrc.apidocumentation.config
 
 import com.google.inject.ImplementedBy
-import play.api.{Configuration, ConfigLoader}
 import javax.inject.Inject
 import uk.gov.hmrc.play.bootstrap.config.{RunMode, ServicesConfig}
 
@@ -60,28 +59,24 @@ trait ApplicationConfig {
   def xmlApiBaseUrl: String
 }
 
-class ApplicationConfigImpl @Inject()(config: Configuration, runMode: RunMode)
-    extends ServicesConfig(config, runMode)
-    with ApplicationConfig {
-
-  val env = runMode.env
-
-  def getConfigDefaulted[A](key: String, default: A)(implicit loader: ConfigLoader[A]) = config.getOptional[A](key)(loader).getOrElse(default)
+class ApplicationConfigImpl @Inject()(config: ServicesConfig, runMode: RunMode) extends ApplicationConfig {
 
   val contactFormServiceIdentifier = "API"
-  val contactPath = getConfigDefaulted(s"$env.contactPath", "")
+  val contactPath = config.getConfString("contactPath", "")
 
-  val analyticsToken = config.getOptional[String](s"$env.google-analytics.token").filterNot(_ == "")
+  val analyticsToken = config.getConfString("google-analytics.token", "") match {
+    case s if !s.isEmpty => Some(s)
+    case _ => None
+  }
+  val analyticsHost = config.getConfString("google-analytics.host", "auto")
 
-  val analyticsHost = getConfigDefaulted(s"$env.google-analytics.host", "auto")
-
-  val developerFrontendUrl = getConfigDefaulted(s"$env.developer-frontend-url", "")
+  val developerFrontendUrl = config.getConfString("developer-frontend-url", "")
 
   val reportAProblemPartialUrl = s"$contactPath/contact/problem_reports_ajax?service=$contactFormServiceIdentifier"
   val reportAProblemNonJSUrl = s"$contactPath/contact/problem_reports_nonjs?service=$contactFormServiceIdentifier"
 
-  val developerFrontendBaseUrl = baseUrl("developer-frontend")
-  val thirdPartyDeveloperUrl = baseUrl("third-party-developer")
+  val developerFrontendBaseUrl = config.baseUrl("developer-frontend")
+  val thirdPartyDeveloperUrl = config.baseUrl("third-party-developer")
 
   /**
    * This value needs to be lazy because it doesn't actually exist in all environments that we deploy to.
@@ -90,35 +85,33 @@ class ApplicationConfigImpl @Inject()(config: Configuration, runMode: RunMode)
    *
    * DO NOT REMOVE
    */
-  lazy val apiDefinitionBaseUrl = baseUrl("api-definition")
+  lazy val apiDefinitionBaseUrl = config.baseUrl("api-definition")
 
-  val securedCookie = getConfigDefaulted(s"$env.cookie.secure", true)
-  val ramlPreviewEnabled = getConfigDefaulted(s"$env.features.ramlPreview", false)
+  val securedCookie = config.getConfBool("cookie.secure", true)
+  val ramlPreviewEnabled = config.getConfBool("features.ramlPreview", false)
   val ramlLoaderRewrites = buildRamlLoaderRewrites
-  val showProductionAvailability = getConfigDefaulted(s"$env.features.showProductionAvailability", false)
-  val showSandboxAvailability = getConfigDefaulted(s"$env.features.showSandboxAvailability", false)
-  val productionApiHost = getString("platform.production.api.host")
-  val productionWwwHost = getString("platform.production.www.host")
+  val showProductionAvailability = config.getConfBool("features.showProductionAvailability", false)
+  val showSandboxAvailability = config.getConfBool("features.showSandboxAvailability", false)
+  val productionApiHost = config.getString("platform.production.api.host")
+  val productionWwwHost = config.getString("platform.production.www.host")
   val productionApiBaseUrl = platformBaseUrl("platform.production.api")
 
-  val sandboxApiHost = getString("platform.sandbox.api.host")
-  val sandboxWwwHost = getString("platform.sandbox.www.host")
+  val sandboxApiHost = config.getString("platform.sandbox.api.host")
+  val sandboxWwwHost = config.getString("platform.sandbox.www.host")
   val sandboxApiBaseUrl = platformBaseUrl("platform.sandbox.api")
   val sandboxWwwBaseUrl = platformBaseUrl("platform.sandbox.www")
 
   val title = "HMRC Developer Hub"
-  val isStubMode = env == "Stub"
-  val xmlApiBaseUrl = getConfigDefaulted(s"$env.xml-api.base-url", "https://www.gov.uk")
+  val isStubMode = runMode.env == "Stub"
+  val xmlApiBaseUrl = config.getConfString("xml-api.base-url", "https://www.gov.uk")
 
   private def buildRamlLoaderRewrites: Map[String, String] = {
-    Map(
-      getConfigDefaulted(s"$env.ramlLoaderUrlRewrite.from", "") ->
-      getConfigDefaulted(s"$env.ramlLoaderUrlRewrite.to", "")
-    )
+    Map(config.getConfString("ramlLoaderUrlRewrite.from", "") ->
+      config.getConfString("ramlLoaderUrlRewrite.to", ""))
   }
 
   private def platformBaseUrl(key: String) = {
-    (getConfigDefaulted(s"$key.protocol", ""), getConfigDefaulted(s"$key.host", "")) match {
+    (config.getConfString(s"$key.protocol", ""), config.getConfString(s"$key.host", "")) match {
       case (p, h) if !p.isEmpty && !h.isEmpty => s"$p://$h"
       case (p, h) if p.isEmpty => s"https://$h"
       case _ => ""
