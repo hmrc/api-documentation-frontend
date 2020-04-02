9ee0b37fbaec472129099b5d6c9b15171452b6a2
Billy Mitchell <26899143+billymitchell14@users.noreply.github.com>
2020-04-02 14:13:37 +0100

    Revert "Api 4279 (#201)"
    This reverts commit 795524cbeada5cfdf806517c116e24f72df941cf.

--- app/uk/gov/hmrc/apidocumentation/services/NavigationService.scala
+++ app/uk/gov/hmrc/apidocumentation/services/NavigationService.scala
@@ -42,7 +42,6 @@ class NavigationService @Inject()(connector: DeveloperFrontendConnector, appConf
   val testingUri = routes.DocumentationController.testingPage().url
   val mtdIntroductionPageUrl = routes.DocumentationController.mtdIntroductionPage().url
   val fraudPreventionPageUrl = routes.DocumentationController.fraudPreventionPage().url
-  val developmentPracticesUrl = routes.DocumentationController.developmentPracticesPage().url
 
   val sidebarNavigationLinks = Seq(
     SidebarLink(label = "Using the Developer Hub", href = gettingStartedUrl,
@@ -52,7 +51,6 @@ class NavigationService @Inject()(connector: DeveloperFrontendConnector, appConf
     SidebarLink(label = "API documentation", href = apiDocumentationUrl, subLinks = previewSublinks()),
     SidebarLink(label = "Testing in the sandbox", href = testingUri),
     SidebarLink(label = "Reference guide", href = referenceGuideUrl),
-    SidebarLink(label = "Development practices", href = developmentPracticesUrl),
     SidebarLink(label = "Terms of use", href = termsOfUseUri),
     SidebarLink(label = "Fraud prevention", href = fraudPreventionPageUrl),
     SidebarLink(label = "Making Tax Digital guides", href = mtdIntroductionPageUrl)
