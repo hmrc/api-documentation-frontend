4ff7ab2e139537ac5e4ed2af8817177205c7f471
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2022-10-19 10:39:16 +0100

    APIS-5967-Docs - Add a comment to a11y script, containing link to confluence setup

--- run_a11y_tests.sh
+++ run_a11y_tests.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
+
+# See https://github.com/hmrc/accessibility-assessment#running-accessibility-assessment-tests-locally for details of preparation required
+#
 sbt -Dbrowser=chrome -Daccessibility.test='true' acceptance:compile acceptance:test
