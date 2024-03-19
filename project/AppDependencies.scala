887c394f25c43c263ff697dfd1d7a2cfcfcdd681
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2024-03-19 03:43:21 +0000

    APIS-6938 Upgrade play frontend to 9.0.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object AppDependencies {
   def apply(): Seq[ModuleID] = compile ++ test
 
-  lazy val bootstrapVersion       = "8.4.0"
+  lazy val bootstrapVersion       = "8.5.0"
   lazy val seleniumVersion        = "4.2.0"
   lazy val jacksonDatabindVersion = "2.10.5.1"
   lazy val jacksonVersion         = "2.10.5"
@@ -17,7 +17,7 @@ object AppDependencies {
     "uk.gov.hmrc"                         %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc"                         %% "play-partials-play-30"      % "9.1.0",
     "uk.gov.hmrc"                         %% "http-metrics"               % "2.8.0",
-    "uk.gov.hmrc"                         %% "play-frontend-hmrc-play-30" % "8.5.0",
+    "uk.gov.hmrc"                         %% "play-frontend-hmrc-play-30" % "9.0.0",
     "uk.gov.hmrc"                         %% "api-platform-api-domain"    % apiDomainVersion,
     "org.typelevel"                       %% "cats-core"                  % "2.10.0",
     "org.commonjava.googlecode.markdown4j" % "markdown4j"                 % "2.2-cj-1.1",
@@ -41,7 +41,7 @@ object AppDependencies {
     "io.cucumber"            %% "cucumber-scala"                  % "5.7.0",
     "org.mockito"            %% "mockito-scala-scalatest"         % "1.17.30",
     "org.jsoup"               % "jsoup"                           % "1.12.1",
-    "uk.gov.hmrc"            %% "ui-test-runner"                  % "0.19.0",
+    "uk.gov.hmrc"            %% "ui-test-runner"                  % "0.22.0",
     "uk.gov.hmrc"            %% "api-platform-test-common-domain" % commonDomainVersion
   ).map(_ % Test)
 }
