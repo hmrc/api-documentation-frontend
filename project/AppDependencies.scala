104424c3ca78ba5e219a302c9200a2ab909707f0
peteslater-ee <28756214+peteslater-ee@users.noreply.github.com>
2025-01-28 08:09:53 +0000

    API-8145 - Updating libs

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object AppDependencies {
   def apply(): Seq[ModuleID] = compile ++ test
 
-  lazy val bootstrapVersion       = "9.1.0"
+  lazy val bootstrapVersion       = "9.7.0"
   lazy val commonDomainVersion    = "0.17.0"
   lazy val apiDomainVersion       = "0.19.1"
 
@@ -13,7 +13,7 @@ object AppDependencies {
     "uk.gov.hmrc"                         %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc"                         %% "play-partials-play-30"      % "10.0.0",
     "uk.gov.hmrc"                         %% "http-metrics"               % "2.8.0",
-    "uk.gov.hmrc"                         %% "play-frontend-hmrc-play-30" % "10.5.0",
+    "uk.gov.hmrc"                         %% "play-frontend-hmrc-play-30" % "11.11.0",
     "uk.gov.hmrc"                         %% "api-platform-api-domain"    % apiDomainVersion,
     "org.typelevel"                       %% "cats-core"                  % "2.10.0",
     "org.commonjava.googlecode.markdown4j" % "markdown4j"                 % "2.2-cj-1.1",
