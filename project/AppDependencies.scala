efce7a05df167dbc6cd5a2c70a1f33837ae87f7a
anjumabbas <khaakpaayali@gmail.com>
2025-08-01 12:45:21 +0100

    APIS-7734 - version bump

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -13,7 +13,7 @@ object AppDependencies {
     "uk.gov.hmrc"                         %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc"                         %% "play-partials-play-30"      % "10.1.0",
     "uk.gov.hmrc"                         %% "http-metrics"               % "2.9.0",
-    "uk.gov.hmrc"                         %% "play-frontend-hmrc-play-30" % "12.7.0",
+    "uk.gov.hmrc"                         %% "play-frontend-hmrc-play-30" % "12.8.0",
     "uk.gov.hmrc"                         %% "api-platform-api-domain"    % apiDomainVersion,
     "org.typelevel"                       %% "cats-core"                  % "2.10.0",
     "org.commonjava.googlecode.markdown4j" % "markdown4j"                 % "2.2-cj-1.1",
