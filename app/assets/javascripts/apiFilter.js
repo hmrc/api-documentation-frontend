2a34ec30da1b5f8d16ffbb183c79318ee967d841
anjumabbas <khaakpaayali@gmail.com>
2025-08-06 10:53:48 +0100

    APIS-7734 - Add ids and fix checkbox click

--- app/assets/javascripts/apiFilter.js
+++ app/assets/javascripts/apiFilter.js
@@ -4,21 +4,24 @@
     const elCategoryFilterCount = document.getElementById('categoryFilterCount');
 
     function updateDocTypeFilterCount() {
-        let selectionCount = document.querySelectorAll('#docTypeFilters input:checked').length;
+        let selectionCount = document.querySelectorAll('#docTypeFilterDetails input:checked').length;
         elDocTypeFilterCount.textContent = '' + selectionCount;
     }
 
     function updateCategoryFilterCount() {
-        let selectionCount = document.querySelectorAll('#categoryFilters input:checked').length;
+        let selectionCount = document.querySelectorAll('#categoryFilterDetails input:checked').length;
         elCategoryFilterCount.textContent = '' + selectionCount;
     }
 
-    let allDocTypeCheckboxes = document.querySelectorAll("#docTypeFilterCheckBox")
+    let docTypeFilterDetails = document.querySelector("#docTypeFilterDetails")
+    let allDocTypeCheckboxes = docTypeFilterDetails.querySelectorAll(".govuk-checkboxes")
+
     for (let i = 0; i < allDocTypeCheckboxes.length; i++) {
         allDocTypeCheckboxes[i].addEventListener("click", updateDocTypeFilterCount);
     }
 
-    let allCategoryCheckboxes = document.querySelectorAll("#categoryFilterCheckBox")
+    let categoryFilterDetails = document.querySelector("#categoryFilterDetails")
+    let allCategoryCheckboxes = categoryFilterDetails.querySelectorAll(".govuk-checkboxes")
     for (let i = 0; i < allCategoryCheckboxes.length; i++) {
         allCategoryCheckboxes[i].addEventListener("click", updateCategoryFilterCount);
     }
