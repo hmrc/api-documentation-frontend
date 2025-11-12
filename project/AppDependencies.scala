f37dc0cdb0c5d8149228897ee6a311ec1175310c
Toby Gilham <623674+Tobias1087@users.noreply.github.com>
2025-11-12 13:57:53 +0000

    APIS-7836 - PRBot comments pt2

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,14 +4,14 @@ import sbt._
 object AppDependencies {
   def apply(): Seq[ModuleID] = compile ++ test
 
-  lazy val bootstrapVersion       = "10.2.0"
+  lazy val bootstrapVersion       = "10.4.0"
   lazy val commonDomainVersion    = "0.19.0"
   lazy val apiDomainVersion       = "0.20.0"
 
   lazy val compile = Seq(
     caffeine,
     "uk.gov.hmrc"                         %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"                         %% "play-partials-play-30"      % "10.1.0",
+    "uk.gov.hmrc"                         %% "play-partials-play-30"      % "10.2.0",
     "uk.gov.hmrc"                         %% "http-metrics"               % "2.9.0",
     "uk.gov.hmrc"                         %% "play-frontend-hmrc-play-30" % "12.20.0",
     "uk.gov.hmrc"                         %% "api-platform-api-domain"    % apiDomainVersion,
