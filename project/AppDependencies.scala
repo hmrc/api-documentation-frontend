be16a6bd63a80a0d7f0a5601bc6ae41e8b835798
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2026-01-13 16:20:31 +0000

    APSR-2122 PR bot actions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object AppDependencies {
   def apply(): Seq[ModuleID] = compile ++ test
 
-  lazy val bootstrapVersion       = "10.4.0"
+  lazy val bootstrapVersion       = "10.5.0"
   lazy val commonDomainVersion    = "0.19.0"
   lazy val apiDomainVersion       = "0.20.0"
 
@@ -13,7 +13,7 @@ object AppDependencies {
     "uk.gov.hmrc"                         %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc"                         %% "play-partials-play-30"      % "10.2.0",
     "uk.gov.hmrc"                         %% "http-metrics"               % "2.9.0",
-    "uk.gov.hmrc"                         %% "play-frontend-hmrc-play-30" % "12.20.0",
+    "uk.gov.hmrc"                         %% "play-frontend-hmrc-play-30" % "12.25.0",
     "uk.gov.hmrc"                         %% "api-platform-api-domain"    % apiDomainVersion,
     "org.typelevel"                       %% "cats-core"                  % "2.10.0",
     "org.commonjava.googlecode.markdown4j" % "markdown4j"                 % "2.2-cj-1.1",
@@ -25,7 +25,7 @@ object AppDependencies {
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"              % bootstrapVersion,
     "org.mockito"            %% "mockito-scala-scalatest"             % "2.0.0",
     "org.jsoup"               % "jsoup"                               % "1.12.1",
-    "uk.gov.hmrc"            %% "ui-test-runner"                      % "0.50.0",
+    "uk.gov.hmrc"            %% "ui-test-runner"                      % "0.52.0",
     "uk.gov.hmrc"            %% "api-platform-common-domain-fixtures" % commonDomainVersion
   ).map(_ % Test)
 }
